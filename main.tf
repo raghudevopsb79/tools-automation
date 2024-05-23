72fd88bffa8fca944490777acf182273256ecf7f
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-23 08:57:57 +0530

    tools spot

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "tools" {
   for_each = var.tools
   source   = "./module"
 
-  name  = each.value["name"]
+  name          = each.value["name"]
   instance_type = each.value["instance_type"]
+  port_no       = each.value["port_no"]
 }
