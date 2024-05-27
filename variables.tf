e109118690db85a6e1a058252353e0ebbcc632fd
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-27 07:58:07 +0530

    Create prometheus setup

--- variables.tf
+++ variables.tf
@@ -5,6 +5,11 @@ variable "tools" {
       instance_type = "t3.small"
       port_no       = "8200"
     }
+    prometheus = {
+      name          = "prometheus"
+      instance_type = "t3.small"
+      port_no       = "9090"
+    }
   }
 }
 
