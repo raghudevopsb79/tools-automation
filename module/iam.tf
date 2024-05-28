cf04a1ecfa134dd585ef5363cae33847c74f3bd6
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-28 07:58:29 +0530

    Create role for tools.

--- module/iam.tf
+++ module/iam.tf
@@ -1,37 +1,33 @@
 resource "aws_iam_role" "main" {
   name = "${var.name}-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+    ]
+  })
 
   inline_policy {
-    name = "inline-policy"
+    name = "my_inline_policy"
 
-    policy = <<EOT
-{
-    "Version": "2012-10-17",
-    "Statement": [
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
         {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": ${var.policy_actions},
-            "Resource": "*"
-        }
-    ]
-}
-EOT
+          Action   = var.policy_actions
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
   }
 
   tags = {
