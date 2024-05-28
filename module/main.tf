223af6bfea8f760c50bdc342295f084e921927b4
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-28 08:16:20 +0530

    Add Iam role to EC2

--- module/main.tf
+++ module/main.tf
@@ -29,6 +29,7 @@ resource "aws_instance" "main" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile   = aws_iam_instance_profile.main.name
 
   instance_market_options {
     market_type = "spot"
