0e76ced657ce507b78c6162d29433df3d5674f68
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-30 07:44:28 +0530

    Grafana

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "tools" {
   default = {
+
     vault = {
       name           = "vault"
       instance_type  = "t3.small"
       port_no        = "8200"
       policy_actions = []
     }
+
     prometheus = {
       name          = "prometheus"
       instance_type = "t3.small"
@@ -15,6 +17,14 @@ variable "tools" {
         "ec2:DescribeAvailabilityZones"
       ]
     }
+
+    grafana = {
+      name          = "grafana"
+      instance_type = "t3.small"
+      port_no       = "3000"
+      policy_actions = []
+    }
+
   }
 }
 
