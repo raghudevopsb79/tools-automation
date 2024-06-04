bc1a5b39703720f5d7df337dfd6e1082bfe66e7d
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-04 07:47:50 +0530

    Add Ingress ports.

--- variables.tf
+++ variables.tf
@@ -4,14 +4,19 @@ variable "tools" {
     vault = {
       name           = "vault"
       instance_type  = "t3.small"
-      port_no        = ["8200"]
+      port_no        = {
+        vault = 8200
+      }
       policy_actions = []
     }
 
     prometheus = {
       name          = "prometheus"
       instance_type = "t3.small"
-      port_no       = ["9090", "9093", "9094"]
+      port_no       = {
+        prometheus = 9090
+        alertmanager = 9093
+      }
       policy_actions = [
         "ec2:DescribeInstances",
         "ec2:DescribeAvailabilityZones"
@@ -21,7 +26,9 @@ variable "tools" {
     grafana = {
       name          = "grafana"
       instance_type = "t3.small"
-      port_no       = ["3000"]
+      port_no       = {
+        grafana = 3000
+      }
       policy_actions = []
     }
 
