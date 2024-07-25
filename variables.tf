30e7b9b74e473410dec2e37bb7cbad8ffbb541de
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-25 08:13:46 +0530

    Terraform Create Vault

--- variables.tf
+++ variables.tf
@@ -1,6 +1,16 @@
 variable "tools" {
   default = {
 
+    vault = {
+      name          = "vault"
+      instance_type = "t3.small"
+      port_no = {
+        vault = 8200
+      }
+      policy_actions = []
+      volume_size    = 20
+    }
+
   }
 }
 
