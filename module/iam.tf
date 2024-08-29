19fa14a38d599f9652bee2d3a38e0bb1c0a4330d
Raghu K <raghuk.vit@gmail.com>
2024-08-29 07:45:58 +0530

    RoboShop Terraform & Kubernetes Code

--- module/iam.tf
+++ module/iam.tf
@@ -41,9 +41,9 @@ resource "aws_iam_instance_profile" "main" {
   role = aws_iam_role.main.name
 }
 
-resource "aws_iam_policy_attachment" "predefined-policies" {
+resource "aws_iam_role_policy_attachment" "predefined-policies" {
   count      = length(var.predefined_policies)
-  name       = aws_iam_role.main.name
+  role       = aws_iam_role.main.name
   policy_arn = "arn:aws:iam::aws:policy/${var.predefined_policies[count.index]}"
 }
 
